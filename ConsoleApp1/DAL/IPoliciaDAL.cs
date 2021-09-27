c41089fe487d165c34d0e29740468f2a67c71681
CRYSTIAN CHINININ BARRANTES <crystian.chininin.barrantes@est.una.ac.cr>
2021-09-27 12:04:40 -0600

    Get tabla Policias

---
+++ ConsoleApp1/DAL/IPoliciaDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace ConsoleApp1.DAL
+{
+    public interface IPoliciaDAL
+    {
+        List<Policias> Get();
+    }
+}
--- ConsoleApp1/DAL/PoliciaDAL.cs
+++ ConsoleApp1/DAL/PoliciaDAL.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp1.DAL
 {
-    class PoliciaDAL
+    public class PoliciaDAL : IPoliciaDAL
     {
-
+        public List<Policias> Get()
+        {
+            List<Policias> lista = new List<Policias>();
+            using (SCAPEntities db = new SCAPEntities())
+            {
+                lista = db.Policias.ToList();
+            }
+            return lista;
+        }
     }
 }
