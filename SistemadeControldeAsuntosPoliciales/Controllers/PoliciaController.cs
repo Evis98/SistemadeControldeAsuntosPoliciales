4f9729936697ff247f62d62337627a6b5db86368
Evis98 <evaduranescobar@gmail.com>
2021-09-26 18:21:27 -0600

    Referencia

---
+++ SistemadeControldeAsuntosPoliciales/Controllers/PoliciaController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BackEnd.DAL;
+
+namespace SistemadeControldeAsuntosPoliciales.Controllers
+{
+    public class PoliciaController : Controller
+    {
+        // GET: Policia
+        //private PoliciaDAL dalPolicia;
+        public ActionResult Index()
+        {
+           
+
+            return View();
+        }
+    }
+}
