aeaec1336d3fe0cbb5974a68d13fdd7ebc0e6304
Mivan221 <70456909+Mivan221@users.noreply.github.com>
2022-04-01 22:43:50 -0600

    Arreglos XD

--- FrontEnd/js/validations.js
+++ FrontEnd/js/validations.js
@@ -247,6 +247,22 @@ function sololetrassimbolos(e) {
 	}
 }
 
+function solonumeros(e) {
+	key = e.keyCode || e.which;
+	teclado = String.fromCharCode(key).toUpperCase();
+	numeros = "1234567890";
+	especiales = "8-37-38-46-164";
+	teclado_especial = false;
+	for (var i in especiales) {
+		if (key == especiales[i]) {
+			teclado_especial = true; break;
+		}
+	}
+	if (numeros.indexOf(teclado) == -1 && !teclado_especial) {
+		return false;
+	}
+}
+
 function sololetrasnumerossimbolos(e) {
 	if (document.getElementById("Mayuscula") != null) {
 		var Mayuscula = document.getElementById("Mayuscula");
